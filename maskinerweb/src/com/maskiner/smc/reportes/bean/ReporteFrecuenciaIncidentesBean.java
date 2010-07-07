66f453f0d2a4f9e0bfdb24a9dfc865bac704af7a
claudioneyram <claudioneyram@8e74a523-6f4e-ea44-8e81-3a01414286d7>
2010-07-07 07:03:09 +0000

    Reporte Frecuencia Incidentes con JPA terminado

--- maskinerweb/src/com/maskiner/smc/reportes/bean/ReporteFrecuenciaIncidentesBean.java
+++ maskinerweb/src/com/maskiner/smc/reportes/bean/ReporteFrecuenciaIncidentesBean.java
@@ -1,65 +1,19 @@
 package com.maskiner.smc.reportes.bean;
 
-import java.io.Serializable;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-
-import org.eclipse.persistence.annotations.NamedStoredProcedureQuery;
-import org.eclipse.persistence.annotations.StoredProcedureParameter;
-
-@Entity
-@NamedStoredProcedureQuery(name = "pr_reporteFrecuenciaIncidentes",
-		procedureName = "pr_reporteFrecuenciaIncidentes",
-		parameters = {
-			@StoredProcedureParameter(queryParameter="cod_cli", name="vcod_cli", type=String.class),
-			@StoredProcedureParameter(queryParameter="annio", name="vanio", type=String.class)
-		},
-		returnsResultSet=true
-/*		,
-		resultClass=ReporteFrecuenciaIncidentesBean.class
-*/
-)
-public class ReporteFrecuenciaIncidentesBean implements Serializable {
+public class ReporteFrecuenciaIncidentesBean {
 	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -2967003664281209474L;
-
-	@Id
-	@Column(name="cod_cli")
 	private String codCliente;
-	
-	@Column(name="raz_soc_cli")
 	private String razonSocialCliente;
-	
-	@Column(name="annio")
 	private String annio;
-
-	@Column(name="num_tar")
 	private String numeroTarjeta;
-	
-	@Column(name="desc_maq")
 	private String descripMaquinaria;
-
-	@Column(name="cant_hidrau")
 	private Long cantidadHidraulicos;
-
-	@Column(name="cant_mecan")
 	private Long cantidadMecanicos;
-
-	@Column(name="cant_elect")
 	private Long cantidadElectricos;
-
-	@Column(name="cant_neumat")
 	private Long cantidadNeumaticos;
-
-	@Column(name="cant_otro")
 	private Long cantidadOtros;
-
-	@Column(name="cant_total")
 	private Long cantidadTotal;
 
 	public String getCodCliente() {
